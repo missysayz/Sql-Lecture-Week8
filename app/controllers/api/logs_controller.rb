b4f2a8134afa70e2c5fa383b5eb08a393ee0a47d
Amanda Garcia <algarcian@outlook.com>
2019-06-28 11:34:30 -0600

    Find by sql queries

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,2 +1,10 @@
 class Api::LogsController < ApplicationController
+
+  def monthly_rental_trend 
+    render json: Log.monthly_rental_trend
+  end 
+
+  def yearly_rental_trend 
+    render json: Log.yearly_rental_trend
+  end 
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,29 @@
 class Log < ApplicationRecord
   belongs_to :customer
   belongs_to :book
+
+  def self.monthly_rental_trend
+    find_by_sql(["
+      SELECT 
+          TO_CHAR(created_at, 'Mon' ) AS rental_date, 
+          TO_CHAR(created_at, 'MM' ) AS rental_month, 
+          COUNT(logs.id) AS rental_count
+      FROM logs 
+      GROUP BY rental_month, rental_date
+      ORDER BY rental_month
+      "])
+  end 
+
+  def self.yearly_rental_trend 
+    find_by_sql(["
+        SELECT 
+            TO_CHAR(created_at, 'Mon-YY') AS rental_date , 
+            TO_CHAR(created_at, 'MM') AS rental_month, 
+              TO_CHAR(created_at, 'YYYY') AS rental_year,
+              COUNT(id) AS rental_count
+        FROM logs 
+        GROUP BY rental_date, rental_month, rental_year
+        ORDER BY rental_month, rental_year
+      "])
+  end 
 end
